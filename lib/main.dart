2b3ccf3aadf25c307266fc158629355dcb6684f1
dduranuceva <dduran@uceva.edu.co>
2025-09-11 08:27:03 -0500

    Fix: actualizar título de la aplicación a 'FORMULARIO GENERAL'

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Formulario UCEVA Nombre y Apellido',
+      title: 'FORMULARIO GENERAL',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
